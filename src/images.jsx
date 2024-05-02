3cfd9e13e2252b54082ab9c7ced6bdaa1aec3647
morquecho-s <mssm011009@gmail.com>
2024-05-02 10:36:43 -0700

    update: actualize las referencias al paquete public (ahora se accede a el sin hacerle referencia)

--- src/images.jsx
+++ src/images.jsx
@@ -1,6 +1,6 @@
-import iconoBorrar from "../public/iconos/borrar.png";
-import iconoCorona from "../public/iconos/corona.png";
-import iconoAlCuadrado from "../public/iconos/x_al_cuadrado.png";
-import iconoRaizCuadrada from "../public/iconos/raiz_cuadrada.png"
+import iconoBorrar from "/iconos/borrar.png";
+import iconoCorona from "/iconos/corona.png";
+import iconoAlCuadrado from "/iconos/x_al_cuadrado.png";
+import iconoRaizCuadrada from "/iconos/raiz_cuadrada.png"
 
 export {iconoBorrar,iconoCorona,iconoAlCuadrado,iconoRaizCuadrada};
