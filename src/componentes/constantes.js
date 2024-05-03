f29bb5af5aa71fc1e0f0a1ca9d6377ecdb700413
morquecho-s <mssm011009@gmail.com>
2024-05-03 11:53:13 -0700

    create: cree al archivo de constantes del componente componentes, ahora se utiliza para hacer mas facil el uso de acciones

---
+++ src/componentes/constantes.js
@@ -0,0 +1,13 @@
+/*
+    
+*/
+export const actions = {
+    exponente_al:{
+        cuadrado:"exp 2",
+        cubo:"exp 3"
+    },
+    raiz:{
+        cuadrada:"-| 2",
+        cubica:"-| 3",        
+    }
+};
